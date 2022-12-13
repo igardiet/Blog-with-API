385e0f1eb907955c9211f05e1b818f83b9376f9f
Victor Jauregui <vicjaudom@hotmail.com>
2022-12-13 17:40:15 +0100

    upload Victor

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 const posts = "http://localhost:3000/posts";
 const users = "http://localhost:3000/users";
 const comments = "http://localhost:3000/comments";
-
-const cardTitle = document.querySelectorAll('.card-title');
 const container = document.querySelector(".row");
 
 fetch(posts)
@@ -11,62 +9,102 @@ fetch(posts)
     .then(json => json.forEach(post=>createCard(post)))
 
 
-  
-
-function createCard (post) {
+function createCard(post) {
     const divCard = document.createElement("div");
     divCard.classList = "card my-5";
     divCard.style.width = "26rem";
-
-    container.append(divCard);
+    divCard.setAttribute("data-bs-toggle", "modal");
+    divCard.setAttribute("data-bs-target", "#exampleModal");
 
     const imgCard = document.createElement("img")
     imgCard.classList = "card-img-top";
     imgCard.setAttribute("src", "assets/fondo-img.jpeg");
     imgCard.setAttribute("alt", "Card image cap");
 
-    divCard.append(imgCard);
+
 
     const cardBody = document.createElement("div")
     cardBody.classList = "card-body";
-
-    imgCard.append(cardBody);
+ 
 
     const cardH5 = document.createElement("h5")
+    cardH5.classList = "card-title";
     cardH5.textContent = post.title;
-
-
-    cardBody.append(cardH5);
-
-    const cardText = document.createElement("p")
-    cardText.classList = "card-text";
-
-    cardH5.append(cardText);
-
-    const cardButton = document.createElement("button")
-    cardButton.classList = "btn btn-primary";
-    cardButton.setAttribute("type", "button");
-    cardButton.setAttribute("data-bs-toggle", "modal");
-    cardButton.setAttribute("data-bs-target", "#exampleModal");
-
-    cardText.append(cardButton);
     
+    container.appendChild(divCard);
+    divCard.appendChild(imgCard);
+    divCard.appendChild(cardBody);
+    cardBody.appendChild(cardH5);
+
+
+
+/**************************************************************************/                      
+
+const divModal = document.createElement("div");
+divModal.classList = "modal fade";
+divModal.setAttribute("id", "exampleModal");
+divModal.setAttribute("tabindex", "1");
+divModal.setAttribute("aria-labelledby", "exampleModalLabel");
+divModal.setAttribute("aria-hidden", "true");
+
+divCard.appendChild(divModal);
+
+const modalDialog = document.createElement("div");
+modalDialog.classList = "modal-dialog";
+
+divModal.appendChild(modalDialog);
+
+const modalContent = document.createElement("div");
+modalContent.classList = "modal-content";
+
+
+modalDialog.appendChild(modalContent);
+
+const modalHeader = document.createElement("div");
+modalHeader.classList = "modal-header";
+
+modalContent.appendChild(modalHeader);
+
+const h1Modal = document.createElement("h1");
+h1Modal.classList = "modal-title fs-5";
+h1Modal.setAttribute("id","exampleModalLabel");
+h1Modal.textContent = post.title;
+
+modalHeader.appendChild(h1Modal);
+
+const buttonModalX = document.createElement("button");
+buttonModalX.classList = "btn-close";
+buttonModalX.setAttribute("type","button");
+buttonModalX.setAttribute("data-bs-dismiss","modal");
+buttonModalX.setAttribute("aria-label","Close");
+
+
+modalHeader.appendChild(buttonModalX);
+
+const modalBody = document.createElement("div");
+modalBody.classList = "modal-body";
+
+
+modalContent.appendChild(modalBody);
+
+const modalFooter = document.createElement("div");
+modalFooter.classList = "modal-footer";
+
+modalContent.appendChild(modalFooter);
+
+const buttonModalGrey = document.createElement("button");
+buttonModalGrey.classList = "btn btn-secondary";
+buttonModalGrey.setAttribute("type","button");
+buttonModalGrey.setAttribute("data-bs-dismiss","modal");
+
+modalFooter.appendChild(buttonModalGrey);
+
+const buttonModalBlue = document.createElement("button");
+buttonModalBlue.classList = "btn btn-primary";
+buttonModalBlue.setAttribute("type","button");
+
+modalFooter.appendChild(buttonModalBlue);
+
+
+
 }
-        
-
-
-
-
-
-
-
-
-
-/*for (let i = 0; i < cardTitle.length; i++) {
-    fetch(posts)
-    .then(res => res.json())
-    .then(data => {
-        cardTitle[i].innerText = data.posts[i].title
-    }) 
-}*/
-
